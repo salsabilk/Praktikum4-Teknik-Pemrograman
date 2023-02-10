75e67038bd7ab690774eead2f109d2b8496b7a43
salsabilk <salsabilkhoirunisa271@gmail.com>
2023-02-10 20:44:15 +0700

    build and import jar

---
+++ JAR_FILE/DemoWeb/src/com/demo/Runner.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import com.polban.jtk.Calculator;
+
+//import com.polban.jtk.Calculator;
+
+//import com.polban.jtk.Calculator;
+
+public class Runner {
+
+	public static void main(String[] args) 
+	{
+
+		Calculator c = new Calculator();
+		
+		System.out.println(c.times(5, 7));
+		
+
+	}
+
+}
---
+++ JAR_FILE/SabilLib/src/com/polban/jtk/Calculator.java
@@ -0,0 +1,9 @@
+package com.polban.jtk;
+
+public class Calculator 
+{
+	public int times(int i, int j) 
+	{
+		return i*j;
+	}
+}
